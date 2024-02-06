dc0b63465ffaa6134c17deabf8b2e343ebdd5491
Arturo Baylon <116782940+ATT5@users.noreply.github.com>
2024-02-06 12:25:01 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        customOrange: "#f07e3a",
+        customPink: "#cd5a91",
+        customBlue: "#2592d2",
+        customGreen: "#45a99d",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
